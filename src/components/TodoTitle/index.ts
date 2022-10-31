77c8adbc26ef509bcad58388a4d00cdb4828ea79
LeeSeungjae00 <seungjae266789@gmail.com>
2022-10-31 21:23:14 +0900

    typescript, recoil 사용해서 todo 만들어 보기

---
+++ src/components/TodoTitle/index.ts
@@ -0,0 +1,2 @@
+import TodoTitle from "./TodoTitle";
+export default TodoTitle;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "./index.css";
+import "./css/color.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      {/* RecoilRoot provider를 이용하여 recoil을 사용가능하도록 설정해줍니다. */}
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
