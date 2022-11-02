3908902048bf60fe81f8b6d1f6240f926d01d22b
LeeSeungjae00 <seungjae266789@gmail.com>
2022-11-02 21:53:06 +0900

    [FIX] index 에 있는 wrapper 컴포넌트 app 으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import TodoTemplate from "components/TodoTemplate/TodoTemplate";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoTemplate from 'components/TodoTemplate/TodoTemplate';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
-  return <TodoTemplate></TodoTemplate>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Router>
+          <TodoTemplate></TodoTemplate>
+        </Router>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
