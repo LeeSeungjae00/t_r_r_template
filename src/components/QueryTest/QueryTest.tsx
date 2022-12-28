3b00df6ade21a77e80e1781e4e69128f60d3ad92
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-28 14:57:30 +0900

    :sparkles: Introduce new features
    비동기 처리하는 부분 AsyncBoundary 로 변경

--- src/components/QueryTest/QueryTest.tsx
+++ src/components/QueryTest/QueryTest.tsx
@@ -1,3 +1,4 @@
+import AsyncBoundary from 'components/AsyncBoundary/AsyncBoundary';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import React, { Suspense } from 'react';
 import { useQueryErrorResetBoundary } from 'react-query';
@@ -7,31 +8,28 @@ export default function QueryTest() {
   const { reset } = useQueryErrorResetBoundary();
   return (
     <div>
-      {/* 로딩 상태인지 판단 불필요 */}
-      <Suspense fallback={<>loading!!!</>}>
-        <ErrorBoundary
-          resetKeys={[]}
-          onReset={() => {
-            reset();
-          }}
-          renderFallback={({ error, reset }) => {
-            return (
-              <>
-                error 발생!!({error.message})
-                <button
-                  onClick={e => {
-                    reset();
-                  }}
-                >
-                  reset
-                </button>
-              </>
-            );
-          }}
-        >
-          <MockList></MockList>
-        </ErrorBoundary>
-      </Suspense>
+      <AsyncBoundary
+        resetKeys={[]}
+        onReset={reset}
+        rejectedFallback={({ error, reset }) => {
+          return (
+            <>
+              error 발생!!({error.message})
+              <button
+                onClick={e => {
+                  reset();
+                }}
+              >
+                reset
+              </button>
+            </>
+          );
+        }}
+        pendingFallback={<>loading!!!</>}
+      >
+        {/* 로딩 상태인지 판단 불필요 */}
+        <MockList></MockList>
+      </AsyncBoundary>
     </div>
   );
 }
