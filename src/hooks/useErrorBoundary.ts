02e51b00aebe66a5ea348da30945c06adcc4908e
LeeSeungjae00 <seungjae266789@gmail.com>
2022-12-30 11:41:39 +0900

    :memo: useErrorBoundary usage 추가

--- src/hooks/useErrorBoundary.ts
+++ src/hooks/useErrorBoundary.ts
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+//usage
+//이 hooks를 통해 반환되는 setError를 catch statement에서 실행하면 컴포넌트 레벨에서 발생한 에러이므로 가장 가까운 ErrorBoundary가 catch하게 된다.
+
 export default function useErrorBoundary<ErrorType extends Error>() {
   const [error, setError] = useState<ErrorType | null>(null);
 
